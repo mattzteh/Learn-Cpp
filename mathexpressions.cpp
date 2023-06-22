a8ae1606599163ad33b4284d1cc5af0a6029e074
mattzteh <mattzteh@gmail.com>
2023-06-21 18:34:19 -0700

    learn prefix and postfix of increment

--- mathexpressions.cpp
+++ mathexpressions.cpp
@@ -35,6 +35,17 @@ int modulo(int num1, int num2) {
     cout<<res<<"\n";
 }
 
+int increment() {
+    int x = 10;
+    int y = x++; // x = 11, y = 10
+    // value of x will be stored in y, THEN it will be incremented by 1
+
+    int z = ++x; // x = 12, z = 12
+    // value of x will be incremented by one, THEN it will be stored in z
+
+    cout<<y<<"\n"<<z<<"\n";
+}
+
 int main() {
 
     int x = 10;
@@ -46,6 +57,7 @@ int main() {
     multiplication(x, y);
     division(z, y);
     modulo(x, y);
+    increment();
 
     return 0;
 }
